045d6bef9ba12b227170766af1b7f55f3b7a3597
Avani <avanitiwari2002@gmail.com>
2025-10-13 15:29:49 -0600

    debugged

--- prog_synth_pipeline/program_synthesis.py
+++ prog_synth_pipeline/program_synthesis.py
@@ -136,7 +136,7 @@ evaluator = ProgramEvaluator()
 recipes_path = "craft/resources/recipes.yaml"
 hints_path = "craft/resources/hints.yaml"
 env_sampler = env_factory.EnvironmentFactory(
-            recipes_path, hints_path, 7, max_steps=100, 
+            recipes_path, hints_path, 0, max_steps=100, 
             reuse_environments=False, visualise=False)
 
 
@@ -272,7 +272,7 @@ def synthesis_baseline():
     client = genai.Client()
     first_failing_funcs = []
     programs = []
-    
+    tasks = ['make[stick]']
     for task in tasks:
         plot =[]
         env = env_sampler.sample_environment(task_name=task)
