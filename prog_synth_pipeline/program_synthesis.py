4837a522b2401fe364fff04cbf3981fdfa88e526
Avani <avanitiwari2002@gmail.com>
2025-10-17 16:01:44 -0600

    reverting back to what we had

--- prog_synth_pipeline/program_synthesis.py
+++ prog_synth_pipeline/program_synthesis.py
@@ -360,7 +360,7 @@ def synthesis_baseline():
             # )
         # Log data to a file
             with open("results/plots/data_make_stick_baseline_new.txt", "w") as log_file:
-                for interactions, reward, wrapped in (c,a, wrapped):
+                for interactions, reward, wrapped in data[-1]:
                     last_prog = programs[-1] if programs else response
                     log_file.write(f"{interactions},{reward},{last_prog}\n")
         # plot_watermark(data, "make[stick]")
